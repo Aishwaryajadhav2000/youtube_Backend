e402bae57878a580a63b4291f502fa767915758c
aishwarya <aishwaryaj1608@gmail.com>
2025-08-03 20:21:51 +0530

    package-lock uncommit

--- index.js
+++ index.js
@@ -10,10 +10,6 @@ app.use(express.json())
 dotenv.config();
 const PORT = process.env.PORT || 8000;
 
-app.get('/' , (req,res) =>{
-    res.send("Welcome to root router")
-})
-
 
 
 //allowed origins of frontend
@@ -31,23 +27,23 @@ app.use(
 
 // MiddleWare
 // For invalid routing
-app.use((req, res) => {
-    res.status(502).json({
-        error: "Invalid routing",
-        message: `The requested URL ( ${req.originalUrl} ) not found on this server.. Please Enter valid URL.`
-    })
-})
+// app.use((req, res) => {
+//     res.status(502).json({
+//         error: "Invalid routing",
+//         message: `The requested URL ( ${req.originalUrl} ) not found on this server.. Please Enter valid URL.`
+//     })
+// })
 
 //  middleware => Error-handling
-app.use((err, req, res, next) => {
-    if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
-        return res.status(417).json({
-            error: "Invalid JSON",
-            message: "Please check your request body. Make sure JSON is properly formatted."
-        });
-    }
-    next(err); // Pass to default error handler if not a JSON error
-});
+// app.use((err, req, res, next) => {
+//     if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+//         return res.status(417).json({
+//             error: "Invalid JSON",
+//             message: "Please check your request body. Make sure JSON is properly formatted."
+//         });
+//     }
+//     next(err); // Pass to default error handler if not a JSON error
+// });
 
 //  Connects to the database and starts the Express server.
 connectDB().then(async () => {
